be13b1f1f94712b75c3c6fa843e62de4ecbb2d42
Prim <tombo136@yahoo.com>
2019-10-22 19:25:39 -0400

    Minor alt

--- Uno_Score_Tracking/GuildCache.cs
+++ Uno_Score_Tracking/GuildCache.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Globalization;
 
 namespace Primbot_v._2.Uno_Score_Tracking {
+    //Regex - adaptable class
     public class GuildCache {
 
         public static List<SocketGuild> connectedGuilds = new List<SocketGuild>();
